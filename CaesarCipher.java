983492060529bb2fc2d9262283438ac6129afd68
Collin Holmquist <collinholmquist@gmail.com>
2020-11-11 15:43:50 -0600

    Create CaesarCipher.java

---
+++ CaesarCipher.java
@@ -0,0 +1,13 @@
+public class CaesarCipher {
+
+   public String encrypt(String message, int shift){
+   
+      return message;
+   }
+   
+   public String decrypt(String message, int shift) {
+   
+      return message; 
+   }
+
+}
